a29f172c2b178d6bef3adce2d137715e5ac3a7e9
StepanMaksimov <stepan.maximov@mytona.com>
2023-02-04 04:14:06 +0800

    player bounds

--- Assets/Scripts/game/map/LoadLevelCollider.cs
+++ Assets/Scripts/game/map/LoadLevelCollider.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class LoadLevelCollider : MonoBehaviour
 {
     public string level;
-    public Vector2 offset = Vector2.zero;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (Player.Instance.map != level) {
             if (other.tag == "Player") {
-                Player.Instance.entrancePosition = new Vector3(other.transform.position.x + offset.x, other.transform.position.y + offset.y, other.transform.position.z);
+                var offset = 5.0f;
+                var coords = other.transform.position;
+                var collider = GetComponent<Collider2D>();
+                var closest = collider.bounds.ClosestPoint(coords);
+                var direction = (closest - coords).normalized;
+                
+                Player.Instance.entrancePosition = new Vector3(
+                    other.transform.position.x + direction.x * offset, 
+                    other.transform.position.y + direction.y * offset, 
+                    other.transform.position.z
+                );
                 SceneManager.LoadSceneAsync(level, LoadSceneMode.Single);
             }
         }
